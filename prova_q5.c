dc938c524935092c5d83d63c5e280f9ccc682d21
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-04-03 15:34:44 -0300

    iniciada questao 5

---
+++ prova_q5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct aluno Aluno;
+struct aluno {
+    char nome[81];
+    float nota;
+};
+
+void ordem_alfabetica (int n, Aluno* v){
+    
+}
+
+int main(){
+    return 0;
+}
