b26b9f1ed8a992147b09440a0337b671bbf2de0a
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-04-05 00:27:33 -0300

    inicia questao 8

---
+++ prova_q8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct no No;
+struct no{
+    char nome[50];
+    No* prox;
+};
+
+typedef struct{
+    No* topo;
+} Pilha;
+
+
+
+int main(){
+    
+    return 0;
+}
