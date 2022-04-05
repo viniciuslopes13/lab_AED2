edf88f9087e651cff01e8512cb1a62900fcbccd6
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-04-05 00:50:17 -0300

    questao 8 iniciada

--- prova_q8.c
+++ prova_q8.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct no No;
 struct no{
-    char nome[50];
+    char caracter;
     No* prox;
 };
 
@@ -11,9 +12,24 @@ typedef struct{
     No* topo;
 } Pilha;
 
-
-
 int main(){
-    
+    int tam;
+    char palavra[100];
+    printf("Digite uma palavra: ");
+    gets(palavra);
+    tam = strlen(palavra);
+    printf("Tamanho = %d",tam);
+    int k=0;
+    int i=0;
+    while(i<(int)(tam/2)){
+        push(palavra[i]);
+        i++;
+    }
+    int temp = tam;
+    while(i<tam){
+        if(pop(pilha)!=palavra[temp-1]) k=1;
+        temp--;
+        i++;
+    }
     return 0;
 }
