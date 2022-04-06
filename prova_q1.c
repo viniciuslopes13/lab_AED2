73fabb6ad439c2fc89d28e89d6735d16b52676a7
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-04-06 19:29:26 -0300

    Adiciona comentários na questao 1

--- prova_q1.c
+++ prova_q1.c
@@ -10,8 +10,10 @@ Matrícula: 2021022958
 
 /*Função que retorna um ponteiro para uma matriz alocada dinamicamente*/
 int **alocarMatriz(int linhas, int colunas){
+    /*Aloca um vetor de ponteiros com base na quantidade de linhas passado por parâmetro*/
 	int **matrix = (int **) malloc(linhas*sizeof(int *));
 	int i;
+    /*Esse laço é percorrido criando um ponteiro para cada linha da matriz*/
 	for(i=0;i<linhas;i++){
 		matrix[i] = (int *) malloc(colunas*sizeof(int));
 	}
@@ -21,9 +23,11 @@ int **alocarMatriz(int linhas, int colunas){
 /*Função que libera espaço de matriz alocada dinamicamente*/
 void desalocarMatriz(int** matriz){
     int i=0;
+    /*O laço é percorrido liberando o ponteiro de cada linha da matriz*/
     for(i=0;i<TAM;i++){
         free(matriz[i]);
     }
+    /*Por fim libeira o ponteiro do vetor de ponteiros*/
     free(matriz);
 }
 
@@ -31,6 +35,7 @@ void desalocarMatriz(int** matriz){
 void exibeMatrizComum(int mat[TAM][TAM]){
     printf("\n");
     int i,j;
+    /*Percorre a matriz passada por parâmetro exibindo os dados*/
 	for(i=0;i<TAM;i++){
         for(j=0;j<TAM;j++){
             printf("%d ", mat[i][j]);
@@ -66,8 +71,13 @@ int** somaMatrizes(int mat1[TAM][TAM], int mat2[TAM][TAM]){
 
 /*Realiza a subtração dos elementos de duas matrizes*/
 int** subtraiMatrizes(int mat1[TAM][TAM], int mat2[TAM][TAM]){
+    /*Aloca uma matriz dinamicamente*/
     int** mat = alocarMatriz(TAM,TAM);
     int i,j;
+    /*
+        Percorre a matriz fazendo que cada posição receba a diferença das respectivas posições 
+        das matrizes passada por parâmetro
+    */
     for(i=0;i<TAM;i++){
         for (j=0; j<TAM; j++){
             mat[i][j] = mat1[i][j] - mat2[i][j];
@@ -80,10 +90,18 @@ int** subtraiMatrizes(int mat1[TAM][TAM], int mat2[TAM][TAM]){
 int** multiplicaMatrizes(int mat1[TAM][TAM], int mat2[TAM][TAM]){
 	int** mat = alocarMatriz(TAM,TAM);
     int lin,col,k,somaprod;
+    /*
+        O encademanto de laços é responsável por fazer a multiplicação de linhas por colunas como
+        acontece na multiplicação de matrizes.
+    */
 	for(lin=0; lin<TAM; lin++){
 	    for(col=0; col<TAM; col++){
 	    	somaprod=0;
 	    	for(k=0; k<TAM; k++){
+                /*
+                    O i(linha) e o j(coluna) permanecem fixos e o k varia. Para matriz1 o k representa o indice
+                    da coluna, já para matriz2 representa o indice da linha. 
+                */
 				somaprod+=mat1[lin][k]*mat2[k][col];
 			}
 	    	mat[lin][col]=somaprod;
