d4f18bb3e7c7571867c600459b7ad8ed8c6700bc
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-04-06 23:32:52 -0300

    adiciona comentários questao 9

--- prova_q9.c
+++ prova_q9.c
@@ -20,6 +20,10 @@ typedef struct{
 
 /*função para empilhar*/
 void push(Pilha *pilha, char d){
+    /*
+        Cria um novo nó, faz o dado do nó receber o parâmetro d, faz o novo nó apontar para o topo da fila
+        e o nó do topo recebe o novo nó.
+    */
     No * novo = (No*) malloc(sizeof(No));
     novo->caracter = d;
     novo->prox = pilha->topo;
@@ -41,16 +45,28 @@ int main(){
     p.topo = NULL;
     printf("Digite uma palavra: ");
     gets(palavra);
+    /*Pega o tamanho da palavra*/
     int tam = strlen(palavra);
     int k=0; 
 	int i=0;
+    /*
+        Empilha cada letra da palavra na pilha, até o metade da palavra.
+    */
     while(i<(tam/2)){
         push(&p,palavra[i]);
         i++;
     }
+    /*
+        Se a palavra for de tamanho par, o while que desempilha pode continuar a contagem a partir de onde o i
+        estava, se for ímpar é necessário fazer um incremento em i.
+    */
     if(tam%2!=0){
     	i++;
 	}
+    /*
+        Esse segundo while é percorrido até o tamanho final da palavra. Aqui a pilha vai sendo desempilhada
+        e cada letra desempilhada vai sendo comparada com a letra do vetor palavra.
+    */
     while(i<tam){
         if(pop(&p)!=palavra[i]){
         	k=1;
